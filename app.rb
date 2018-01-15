3dddb97dc48e7fab3a45ba09aa9ff13f37f2a55c
Ed Perkins <ed@edperkins.co.uk>
2018-01-15 15:43:24 +0000

    fence in a cat

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'shotgun'
 # $ shotgun app.rb -p 4567
 
+
+
 set :session_secret, 'super secret'
 
 get '/' do
@@ -14,4 +16,10 @@ end
 
 get '/tomato' do
   "blah blah"
+end
+
+get '/cat' do
+  "<div>
+  <img src='http://bit.ly/1eze8aE' style='border: 3px dashed red'>
+  </div>"
 end
